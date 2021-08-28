0f79689e070bc32c9d08fc113d207093d1e82bbf
MachuaNjoroge <machuavyn@gmail.com>
2021-08-28 09:37:47 +0300

    Adding subject model

--- db/migrate/20210828060800_create_subject.rb
+++ db/migrate/20210828060800_create_subject.rb
@@ -1,9 +1,18 @@
 class CreateSubject < ActiveRecord::Migration[6.1]
-  def change
+  def up
     create_table :subjects do |t|
-
+      t.string "name"
+      t.integer "position"
+      t.boolean "visible", default:false
+      
       t.timestamps
+      # t.datetime "created_at"
+      # t.datetime "updated_at"
     end
+  end
+
+  def down
+    drop_table :subjects
   end
 
 end
