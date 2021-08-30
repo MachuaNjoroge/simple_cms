e2338c44dd7869795f8831fe8e293a74832bdfd1
MachuaNjoroge <machuavyn@gmail.com>
2021-08-30 20:56:41 +0300

    Adding rich association traversal

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,7 @@
 class Section < ApplicationRecord
 
     has_many :section_edits
+    has_many :admin_users, :through => :section_edits
 
     scope :with_content_type, lambda {|ctype|
         where(content_type:ctype)
